6205f313f3584fc96cf0f8892a466249e20df73f
Voiislav <vojmitrovic@gmail.com>
2023-12-30 16:53:11 +0100

    feat: add crash-test

--- backend/app.js
+++ backend/app.js
@@ -30,6 +30,12 @@ app.use(cookieParser());
 
 app.use(requestLogger);
 
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+}); 
+
 app.post('/signin', login);
 
 app.post('/signup', createUser);
