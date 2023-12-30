99601897faf070fb8975ea6b2d537f0aecd876b9
Voiislav Mitrovich <133046530+Voiislav@users.noreply.github.com>
2023-12-30 21:47:08 +0100

    Delete frontend directory

--- frontend/src/components/ProtectedRoute.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { Navigate } from "react-router-dom";
-
-const ProtectedRouteElement = ({ loggedIn, element }) => {
-  return loggedIn ? element : <Navigate to="/sign-up" replace />;
-};
-
-export default ProtectedRouteElement;
